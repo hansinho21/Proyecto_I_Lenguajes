38790c28f66f0f1e02a62fa68422d9d7a6debbd8
hansinho21 <hansinho21@users.noreply.github.com>
2019-05-28 23:16:19 -0600

    crud Orden

--- Proyecto1/src/main/java/com/proyecto1/ecommerce/domain/Orden.java
+++ Proyecto1/src/main/java/com/proyecto1/ecommerce/domain/Orden.java
@@ -7,9 +7,15 @@ public class Orden {
 	private int idOrden;
 	private Date fechaOrden;
 	private Date fechaEnvio;
+	private float valorEnvio;
+	private float valorProductos;
 	private float valorTotal;
+	private Cliente cliente;
+	private EstadoOrden estadoOrden;
 	
 	public Orden() {
+		cliente = new Cliente();
+		estadoOrden = new EstadoOrden();
 	}
 
 	public int getIdOrden() {
@@ -36,6 +42,22 @@ public class Orden {
 		this.fechaEnvio = fechaEnvio;
 	}
 
+	public float getValorEnvio() {
+		return valorEnvio;
+	}
+
+	public void setValorEnvio(float valorEnvio) {
+		this.valorEnvio = valorEnvio;
+	}
+
+	public float getValorProductos() {
+		return valorProductos;
+	}
+
+	public void setValorProductos(float valorProductos) {
+		this.valorProductos = valorProductos;
+	}
+
 	public float getValorTotal() {
 		return valorTotal;
 	}
@@ -43,6 +65,31 @@ public class Orden {
 	public void setValorTotal(float valorTotal) {
 		this.valorTotal = valorTotal;
 	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public EstadoOrden getEstadoOrden() {
+		return estadoOrden;
+	}
+
+	public void setEstadoOrden(EstadoOrden estadoOrden) {
+		this.estadoOrden = estadoOrden;
+	}
+
+	@Override
+	public String toString() {
+		return "Orden [idOrden=" + idOrden + ", fechaOrden=" + fechaOrden + ", fechaEnvio=" + fechaEnvio
+				+ ", valorEnvio=" + valorEnvio + ", valorProductos=" + valorProductos + ", valorTotal=" + valorTotal
+				+ ", cliente=" + cliente.getIdCliente() + ", estadoOrden=" + estadoOrden.getIdEstadoOrden() + "]";
+	}
+
+	
 	
 	
 	
